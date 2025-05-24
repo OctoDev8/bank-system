e7816bb7b3cb3b5b0aa4fe885f8d0e8427d1d656
OctoDev8 <gabrielmejorada@gmail.com>
2025-05-24 16:20:02 -0600

    Pushing the actual foundation cuz me rusty in git lol

---
+++ main.c
@@ -0,0 +1,26 @@
+//NO AI HELP! ANYTHING ELSE IS ALLOWED
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct bankUser{
+    char *option;
+    int userName;
+    int password;
+}user;
+
+*user = NULL;
+int length = 0;
+
+//Access struct and make this function able to save username and password
+void createAccount(const char *option){
+    
+}
+
+int main(){
+    printf("Bank System CLI Project\n");
+    printf("Choose your options by inputting a number\n");
+    printf("1. Create an account\n");
+    printf("2. Close an account\n");
+    printf("3. Deposit money to an account\n");
+    printf("4. Withdraw money from an account");
+}
